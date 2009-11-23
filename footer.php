1167148ffcc45919fcd729161d866004ed1e7b70
Gökmen Görgen <gkmngrgn@gmail.com>
2009-11-24 00:32:03 +0200

    Remove icons, update footer info.

--- footer.php
+++ footer.php
@@ -1,13 +1,7 @@
 </div>
 <p>
 <div id="footer">
-<?php echo("2008 (c) Gökmen Görgen ~ Birtakım haklar <a href=\"http://creativecommons.org/licenses/by-nc-sa/2.5/\">saklıdır</a>."); ?><br>
-<a href="http://jigsaw.w3.org/css-validator/">
-    <img style="border:0;width:88px;height:31px"
-        src="http://jigsaw.w3.org/css-validator/images/vcss-blue"
-        alt="Valid CSS!" />
-</a>
-<a href="http://validator.w3.org/feed/check.cgi?url=http%3A//www.gokmengorgen.net/gunluk/%3Ffeed%3Drss2"><img src="http://validator.w3.org/feed/images/valid-rss.png" alt="[Valid RSS]" title="Validate my RSS feed" /></a>
-<a href="http://www.gnu.org"><img src="http://www.gnu.org/graphics/gnubanner-2.png" alt="Gnu Is Not Unix!" title="For Freedom!"/></a>
+Bu site, gücünü <a href="http://wordpress.com/">WordPress</a>'ten alır.<br />
+<?php echo("2008 (c) Gökmen Görgen ~ Birtakım haklar <a href=\"http://creativecommons.org/licenses/by-nc-sa/2.5/\">saklıdır</a>."); ?>
 </p>
 </div>
